8c0fc9011aa7d8fa7ddb5472b523990374f35fc9
Ubuntu <ubuntu@ip-172-31-48-139.ec2.internal>
2022-08-18 17:52:50 +0000

    already done with kubernetes run

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=doksyilji/devops-proj
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+cat ~/environment/my_password.txt | docker login --username doksyilji --password-stdin
+kubectl create deploy m1 --image=${dockerpath}:m1_remote_0.0.1
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+POD=$(kubectl get pod -l app=m1 -o jsonpath="{.items[0].metadata.name}")
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/$POD --address 0.0.0.0 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=doksyilji/devops-proj
 
 # Step 2:  
 # Authenticate & tag
+cat ~/environment/my_password.txt | docker login --username doksyilji --password-stdin
+docker tag m1:latest ${dockerpath}:m1_remote_0.0.1
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}:m1_remote_0.0.1
